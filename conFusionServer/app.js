7af59325df5d3e8acf5639fc12f2c335f46e7bcb
Ie Khing <iekhing@mobilewalla.com>
2021-04-09 22:45:34 +0800

    authentication

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,41 +3,213 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+var session = require('express-session');
+var passport = require('passport');
+
+var config = require('./config');
+
+
+var FileStore = require('session-file-store')(session);
+
 
 
 
 var app = express();
 
+
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//basic auth
+// function auth (req, res, next) {
+
+//   if (!req.signedCookies.user) {
+//     var authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         var err = new Error('You are not authenticated!');
+//         res.setHeader('WWW-Authenticate', 'Basic');              
+//         err.status = 401;
+//         next(err);
+//         return;
+//     }
+//     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+//     var user = auth[0];
+//     var pass = auth[1];
+//     if (user == 'admin' && pass == 'password') {
+//         res.cookie('user','admin',{signed: true});
+//         next(); // authorized
+//     } else {
+//         var err = new Error('You are not authenticated!');
+//         res.setHeader('WWW-Authenticate', 'Basic');              
+//         err.status = 401;
+//         next(err);
+//     }
+//   }
+//   else {
+//       if (req.signedCookies.user === 'admin') {
+//           next();
+//       }
+//       else {
+//           var err = new Error('You are not authenticated!');
+//           err.status = 401;
+//           next(err);
+//       }
+//   }
+// }
+
+
+//cookie
+// function auth (req, res, next) {
+
+//   if (!req.signedCookies.user) {
+//     var authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         var err = new Error('You are not authenticated!');
+//         res.setHeader('WWW-Authenticate', 'Basic');              
+//         err.status = 401;
+//         next(err);
+//         return;
+//     }
+//     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+//     var user = auth[0];
+//     var pass = auth[1];
+//     if (user == 'admin' && pass == 'password') {
+//         res.cookie('user','admin',{signed: true});
+//         next(); // authorized
+//     } else {
+//         var err = new Error('You are not authenticated!');
+//         res.setHeader('WWW-Authenticate', 'Basic');              
+//         err.status = 401;
+//         next(err);
+//     }
+//   }
+//   else {
+//       if (req.signedCookies.user === 'admin') {
+//           next();
+//       }
+//       else {
+//           var err = new Error('You are not authenticated!');
+//           err.status = 401;
+//           next(err);
+//       }
+//   }
+// }
+
+//session
+// function auth (req, res, next) {
+//   console.log(req.session);
+
+//   if (!req.session.user) {
+//       var authHeader = req.headers.authorization;
+//       if (!authHeader) {
+//           var err = new Error('You are not authenticated!');
+//           res.setHeader('WWW-Authenticate', 'Basic');                        
+//           err.status = 401;
+//           next(err);
+//           return;
+//       }
+//       var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+//       var user = auth[0];
+//       var pass = auth[1];
+//       if (user == 'admin' && pass == 'password') {
+//           req.session.user = 'admin';
+//           next(); // authorized
+//       } else {
+//           var err = new Error('You are not authenticated!');
+//           res.setHeader('WWW-Authenticate', 'Basic');
+//           err.status = 401;
+//           next(err);
+//       }
+//   }
+//   else {
+//       if (req.session.user === 'admin') {
+//           console.log('req.session: ',req.session);
+//           next();
+//       }
+//       else {
+//           var err = new Error('You are not authenticated!');
+//           err.status = 401;
+//           next(err);
+//       }
+//   }
+// }
+
+
+// //mongoose
+// function auth (req, res, next) {
+//   console.log(req.session);
+
+//   if(!req.session.user) {
+//     var err = new Error('You are not authenticated!');
+//     err.status = 403;
+//     return next(err);
+
+//   }
+//   else {
+//     if (req.session.user === 'authenticated') {
+//       next();
+//     }
+//     else {
+//       var err = new Error('You are not authenticated!');
+//       err.status = 403;
+//       return next(err);
+//     }
+//   }
+// }
+
+//passport
+function auth (req, res, next) {
+  console.log(req.user);
+  console.log(req);
+
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
+  }
+  else {
+        next();
+  }
+}
+
+app.use(auth);
 
 
 
 
-
-
-
-var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
-const mongoose = require('mongoose');
-const Dishes = require('./models/dishes');
-
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
